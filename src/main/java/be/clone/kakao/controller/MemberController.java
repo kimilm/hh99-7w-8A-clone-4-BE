ae2b828880c600288c3437b9055ade573147cc41
kimilm <gnlfla7416@gmail.com>
2022-08-21 15:36:38 +0900

    feature: 회원가입 기능 구현

--- src/main/java/be/clone/kakao/controller/MemberController.java
+++ src/main/java/be/clone/kakao/controller/MemberController.java
@@ -1,8 +1,14 @@
 package be.clone.kakao.controller;
 
+import be.clone.kakao.domain.member.dto.SignupRequestDto;
 import be.clone.kakao.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -10,4 +16,16 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    // 회원가입
+    @PostMapping("/api/signup")
+    public ResponseEntity<?> signup(@RequestBody SignupRequestDto requestDto) {
+
+        Long memberId = memberService.signup(requestDto);
+
+        return ResponseEntity.ok()
+                .body(Map.of(
+                        "memberId", memberId,
+                        "msg", "회원가입 성공"
+                ));
+    }
 }
--- src/main/java/be/clone/kakao/service/MemberService.java
+++ src/main/java/be/clone/kakao/service/MemberService.java
@@ -1,6 +1,9 @@
 package be.clone.kakao.service;
 
+import be.clone.kakao.domain.member.Member;
+import be.clone.kakao.domain.member.dto.SignupRequestDto;
 import be.clone.kakao.repository.MemberRepository;
+import be.clone.kakao.util.MemberUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +13,15 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    // 회원가입
+    public Long signup(SignupRequestDto requestDto) {
+        // Todo 패스워드 인코딩 수행
+        String encodedPassword = requestDto.getPassword();
+        // 기본 이미지
+        String defaultProfilePic = MemberUtils.getPic();
+        // 맴버 객체 생성
+        Member member = Member.of(requestDto, encodedPassword, defaultProfilePic);
+        // 저장 후 아이디 리턴
+        return memberRepository.save(member).getMemberId();
+    }
 }
