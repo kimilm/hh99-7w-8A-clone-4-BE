ea4685e543b09e55c8a932321249ea82d738a87f
kimilm <gnlfla7416@gmail.com>
2022-08-23 22:39:09 +0900

    update: 소켓 연결시 채팅방 리스트 리턴

--- src/main/java/be/clone/kakao/controller/StompChatController.java
+++ src/main/java/be/clone/kakao/controller/StompChatController.java
@@ -1,10 +1,11 @@
 package be.clone.kakao.controller;
 
-import be.clone.kakao.domain.chat.dto.ChatListRequestDto;
-import be.clone.kakao.domain.chat.dto.ChatRequestDto;
-import be.clone.kakao.domain.member.Member;
+import be.clone.kakao.domain.Room.dto.RoomMasterResponseDto;
+import be.clone.kakao.domain.chat.dto.ChatDto;
 import be.clone.kakao.service.ChatService;
+import be.clone.kakao.service.RoomService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -12,33 +13,31 @@ import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class StompChatController {
 
     private final SimpMessagingTemplate template; //특정 Broker로 메세지를 전달
     private final ChatService chatService;
+    private final RoomService roomService;
 
-    //Client가 SEND할 수 있는 경로
-    //stompConfig에서 설정한 applicationDestinationPrefixes와 @MessageMapping 경로가 병합됨
     //"/pub/chat/enter"
     //메세지 받았을 때
-    @MessageMapping(value = "/room/{roomId}")
-    public void message(@DestinationVariable String roomId, ChatRequestDto message){
+    @MessageMapping(value = "/chat/room/{roomId}")
+    public void message(@DestinationVariable String roomId, ChatDto message) {
         //채팅 저장
-        chatService.saveChat(Long.parseLong(roomId),message);
+        chatService.saveChat(Long.parseLong(roomId), message);
+        log.info("pub success " + message.getContent());
         //채팅방에 정보 전달
         template.convertAndSend("/sub/chat/room/" + roomId, message);
-        //채팅방 리스트에 정보 전달
-        ChatListRequestDto listDto = new ChatListRequestDto(Long.parseLong(roomId),message);
-        template.convertAndSend("/sub/room/" + roomId, listDto);
     }
 
-    //채팅방리스트 내 체팅방 종류 조정
-    @MessageMapping(value = "/room/member/{memberId}")
-    public void NewRoom(@DestinationVariable String memberId, ChatRequestDto message){
-        template.convertAndSend("/sub/room/member/" + memberId, message);
+    // 사용자가 속한 채팅방 리스트 불러오기
+    @MessageMapping(value = "/room/{memberId}")
+    public void enterRoom(@DestinationVariable String memberId) {
+        List<RoomMasterResponseDto> rooms = roomService.getRoom(memberId);
+        log.info("room list");
+        template.convertAndSend("/sub/room/" + memberId, rooms);
     }
-
-
 }
