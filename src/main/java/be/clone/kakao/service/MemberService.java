7356bf62e98bfe9c14b844375aea3068552c440e
Rzreo <zuzu5313@naver.com>
2022-08-25 09:51:09 +0900

    안쓰는 변수 제거

--- src/main/java/be/clone/kakao/service/MemberService.java
+++ src/main/java/be/clone/kakao/service/MemberService.java
@@ -125,7 +125,7 @@ public class MemberService {
         return member.getMemberId();
     }
 
-    public LoginResponseDto kakaoLogin(String code, HttpServletResponse response) throws JsonProcessingException {
+    public LoginResponseDto kakaoLogin(String code) throws JsonProcessingException {
         // 1. "인가 코드"로 "액세스 토큰" 요청
         String accessToken = getKakaoAccessToken(code);
         // 2. 토큰으로 카카오 API 호출
